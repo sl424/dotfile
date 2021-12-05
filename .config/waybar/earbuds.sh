c2c3ce5a6b3cd62e2e6eb25c785c906c073721bf
slin72 <slin72@asu.edu>
2021-12-05 14:27:38 -0800

    refactor code

--- .config/waybar/earbuds.sh
+++ .config/waybar/earbuds.sh
@@ -1,46 +1,34 @@
 #!/bin/sh
-BUDS_STATUS=`earbuds status -o json -q`
+BUDS_STATUS=$(earbuds status -o json -q)
+REQ_STATUS=$(echo $BUDS_STATUS | jq '.status' -r)
 
-REQ_STATUS=`echo $BUDS_STATUS | jq '.status' -r`
 if [ "$REQ_STATUS" != "success" ]; then
-	echo "{\"text\": \" -- :  --\", \"class\" : \"none\", \"percentage\" : \" -- :  -- \"}"
-    exit 0
+	printf '{"text":" -- |  --","class":"none","percentage":0}\n'
+	exit 0
 fi
+case=' '$(echo $BUDS_STATUS | jq -r '.payload.batt_case')
 
-LEFT=$(echo $BUDS_STATUS | jq -r '.payload.batt_left')
-LS=$(echo $BUDS_STATUS | jq '.payload.placement_left')
-
-RIGHT=$(echo $BUDS_STATUS | jq -r '.payload.batt_right')
-RS=$(echo $BUDS_STATUS | jq '.payload.placement_right')
-
-case $LS in
-	1)
-		LEFT=" $LEFT"
-		;;
-	2)
-		LEFT="﫽 $LEFT"
-		;;
-	3)
-		LEFT=" $LEFT"
-		;;
-	*)
-		LEFT=" $LEFT"
-		;;
-esac
-
-case $RS in
-	1)
-		RIGHT=" $RIGHT"
-		;;
-	2)
-		RIGHT="﫽 $RIGHT"
-		;;
-	3)
-		RIGHT=" $RIGHT"
-		;;
-	*)
-		RIGHT=" $RIGHT"
-		;;
-esac
-
-echo "{\"text\":\"$LEFT | $RIGHT\", \"class\":\"bluetooth\", \"percentage\":\"$LEFT | $RIGHT\"}"
+left=''
+right=''
+bat=()
+for i in "left" "right"; do
+	BAT=$(echo $BUDS_STATUS | jq -r '.payload.batt_'$i)
+	bat+=($BAT)
+	POS=$(echo $BUDS_STATUS | jq '.payload.placement_'$i)
+	case $POS in
+		1)
+			eval "$i=' '$BAT"
+			;;
+		2)
+			eval "$i=' '$BAT"
+			;;
+		3)
+			eval "$i=' '$BAT"
+			;;
+		*)
+			eval "$i=' '$BAT"
+			;;
+	esac
+done
+min=$( printf "%s\n" "${bat[@]}" | awk '$0>0' | sort -n | head -n1 )
+printf '{"text":"%s | %s","class":"bluetooth","percentage":%d}\n' "$left" "$right" "$min"
